82a0f80a39245e08455b9b1454238b1c875dd7c9
dcmotocorp <dipenpatell2496.com>
2023-05-25 10:17:51 +0530

    werew

--- brand/test3.py
+++ brand/test3.py
@@ -6,36 +6,23 @@ repo_path = '/path/to/repository'
 # Open the repository
 repo = pygit2.Repository(os.path.dirname(os.path.abspath(__file__)))
 
-# Get the master branch
+# Checkout the master branch
 master_branch = repo.branches['master']
+repo.checkout(master_branch)
 
 # Iterate over all branches
-for branch_name in repo.branches.remote:
-    # Skip the master branch
-    if branch_name == 'master':
-        continue
+for branch in repo.branches.local:
+    if branch.name != 'master':
+        # Checkout the branch
+        repo.checkout(branch)
 
-    # Get the branch reference
-    branch_ref = repo.branches[branch_name]
+        # Rebase the branch onto the latest changes in master
+        rebase_options = pygit2.GitRebaseOptions()
+        rebase_options.onto = master_branch.target
+        repo.rebase(branch.target, options=rebase_options)
 
-    # Create a temporary branch for the rebase
-    temp_branch_name = f'temp/{branch_name}'
-    temp_branch_ref = repo.create_branch(temp_branch_name, branch_ref.target)
-    temp_branch_ref.upstream = master_branch.target
+# Checkout the master branch again
+repo.checkout(master_branch)
 
-    # Checkout the temporary branch
-    repo.checkout(temp_branch_name)
-
-    # Perform the rebase manually
-    repo.merge(temp_branch_ref.target)
-
-    # Update the branch reference with the rebased commits
-    branch_ref.set_target(temp_branch_ref.target)
-
-    # Push the rebased branch to the GitLab repository
-    remote_name = 'origin'
-    remote = repo.remotes[remote_name]
-    remote.push([f'{branch_name}:{branch_name}'])
-
-    # Delete the temporary branch
-    repo.branches.delete(temp_branch_name)
+# Print a success message
+print("Rebase completed successfully.")
